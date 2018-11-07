fa6f79a82f1f121c04b6df581b6a0bd9c2b2d0af
dstags-LO <david.stagnitto@gmail.com>
2018-11-07 11:43:03 -0500

    Explicitly set ActiveRecord::Migration version

--- db/migrate/20180827051522_add_store_price_book.rb
+++ db/migrate/20180827051522_add_store_price_book.rb
@@ -1,4 +1,4 @@
-class AddStorePriceBook < ActiveRecord::Migration
+class AddStorePriceBook < ActiveRecord::Migration[4.2]
   def change
     remove_column :spree_price_books, :store_id
     remove_index :spree_price_books, :store_id
