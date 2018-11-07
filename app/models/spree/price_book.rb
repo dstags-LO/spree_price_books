1d29be54bc74d477ece1805b358047285940392c
dstags-LO <david.stagnitto@gmail.com>
2018-11-07 16:26:15 -0500

    bump

--- app/models/spree/price_book.rb
+++ app/models/spree/price_book.rb
@@ -5,8 +5,11 @@ class Spree::PriceBook < ActiveRecord::Base
   belongs_to :role, class_name: 'Spree::Role'
 
   has_many :prices
+
+
   has_many :variants, through: :prices
   has_many :products, -> { uniq }, through: :variants
+  has_many :stores
   has_many :store_price_books
   has_many :stores, through: :store_price_books
 
